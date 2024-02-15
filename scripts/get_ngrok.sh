6e0cb710684bc539186e3c366273db9a695160ea
appskary <160188918+appskary@users.noreply.github.com>
2024-02-15 15:49:12 -0800

    Update get_ngrok.sh

--- scripts/get_ngrok.sh
+++ scripts/get_ngrok.sh
@@ -2,9 +2,9 @@
 
 command -v ./ngrok >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
-    echo ngrok is not found, installing...
-    wget -q -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-    unzip -qq -n ngrok-stable-linux-amd64.zip
+    echo ngrok is not found, scrutinizing, installing...
+    wget -q -nc https://archive.org/download/ngrok-v3-stable-linux-amd64/ngrok-v3-stable-linux-amd64.zip
+    unzip -qq -n ngrok-v3-stable-linux-amd64.zip
     echo Done!
 fi
 
